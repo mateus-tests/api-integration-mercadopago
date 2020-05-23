d0e2c8f21159477583e0f2763c32b1fd55b3c3ad
Computrailer <letsgood56@gmail.com>
2020-05-23 16:43:56 -0300

    repair fix

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(express.json());
+//app.use(express.json());
+app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 app.use(cors());
 
