ed73fb0e3201ec28758e407c29e23cde907b80e2
mateus-tests <65824449+mateus-tests@users.noreply.github.com>
2020-06-04 15:40:55 -0300

    Update EmailServices.js

--- services/EmailServices.js
+++ services/EmailServices.js
@@ -22,8 +22,8 @@ module.exports = {
                 from : `RpgZone <${process.env.EMAIL}>`,
                 to : `${email}`,
                 subject : "Email confirmation for sign up into Rpg Zone",
-                text : `Hello new user with name of ${nickName}, nice meet to you, this email are  confirmation email for sign up on to rpg Zone`,
-                html : `<p>Hello new user with name of ${nickName}, nice meet to you, this email are  confirmation email for sign up on to rpg Zone The link for to be approved sign up in rpgZone is => </p></br><a href='https://mateus-tests.github.io/rpgzoneFront/signup.html?token=${token}'>Here same!!!</a>`
+                text : `Hello new user with name of ${nickName}, nice to meet you this email is a confirmation email for sign up on to rpg Zone`,
+                html : `<p>Hello new user with name of ${nickName}, nice to meet you this email is a confirmation email for sign up on to rpg Zone The link for to be approved sign up in rpgZone is => </p></br><a href='https://mateus-tests.github.io/rpgzoneFront/signup.html?token=${token}'>Here same!!!</a>`
             }).catch(err => {
                 console.log(err);
                 res.json({status : 400});
